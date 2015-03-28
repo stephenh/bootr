b8754198b98119bec334d8053079244e4bea853b
Stephen Haberman <stephen@exigencecorp.com>
2015-03-28 16:39:08 -0500

    Get a basic/happy path unit test working.

--- src/test/java/bootr/BootrTest.java
+++ src/test/java/bootr/BootrTest.java
@@ -1,42 +1,105 @@
 package bootr;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.vfs2.CacheStrategy;
 import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemException;
-import org.apache.commons.vfs2.cache.NullFilesCache;
+import org.apache.commons.vfs2.FileType;
+import org.apache.commons.vfs2.FileTypeSelector;
+import org.apache.commons.vfs2.cache.DefaultFilesCache;
+import org.apache.commons.vfs2.impl.DefaultFileReplicator;
 import org.apache.commons.vfs2.impl.DefaultFileSystemManager;
+import org.apache.commons.vfs2.impl.FileContentInfoFilenameFactory;
+import org.apache.commons.vfs2.provider.local.DefaultLocalFileProvider;
 import org.apache.commons.vfs2.provider.ram.RamFileProvider;
+import org.apache.commons.vfs2.provider.zip.ZipFileProvider;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BootrTest {
 
-  private DefaultFileSystemManager fs = new DefaultFileSystemManager();
+  private final DefaultFileSystemManager fs = new DefaultFileSystemManager();
+  private final Map<String, String> config = new HashMap<String, String>();
+  private final Inputter inputter = new Inputter() {
+    public String ask(String key, String detail) throws Exception {
+      return config.get(key);
+    }
+  };
+  private FileObject localDir;
+  private Bootr b;
 
   @Before
   public void setup() throws Exception {
-    fs.setFilesCache(new NullFilesCache());
     fs.setCacheStrategy(CacheStrategy.MANUAL);
+    fs.setFilesCache(new DefaultFilesCache());
+    fs.setReplicator(new DefaultFileReplicator());
+    fs.setFileContentInfoFactory(new FileContentInfoFilenameFactory());
+    fs.addMimeTypeMap("application/zip", "zip");
+
+    fs.addProvider("zip", new ZipFileProvider());
     fs.addProvider("https", new RamFileProvider());
-    
-    FileObject o2 = fs.resolveFile("https://foo.github.com/bar.zip");
-    OutputStream out = o2.getContent().getOutputStream();
-    IOUtils.write("bar", out);
-    out.close();
-    
-    FileObject o = fs.resolveFile("https://foo.github.com/bar.zip");
-    System.out.println(o);
-    System.out.println(o.getContent().getSize());
+    fs.addProvider("ram", new RamFileProvider());
+    fs.addProvider("file", new DefaultLocalFileProvider());
+
+    localDir = fs.resolveFile("ram:/");
+    localDir.createFolder();
+    b = new Bootr(fs, inputter, localDir, new GithubProject("user", "project", "master"));
   }
 
   @Test
-  public void downloadProjectZipFromGitHub() throws FileSystemException {
-    // FileSystem urls = fs.createFileSystem("url", fs.resolveFile("ram://"));
-    // System.out.println(fs.resolveFile("url://foo"));
+  public void downloadProjectZipFromGitHub() throws Exception {
+    // given a basic project layout
+    FileObject tmpDir = fs.resolveFile("ram:/tmp/");
+    writeToFile(tmpDir.resolveFile("project-master/rootFile.txt"), "1234");
+    writeToFile(tmpDir.resolveFile("project-master/dir1/dirFile.txt"), "1234");
+
+    // that is upload to github
+    FileObject zipFile = fs.resolveFile("https://www.github.com/user/project/archive/master.zip");
+    writeToZipFile(zipFile, tmpDir);
+
+    // when extracted
+    b.run();
+
+    // we copied the file as is
+    assertThat(readFromFile(localDir.resolveFile("project/rootFile.txt")), is("1234"));
+    assertThat(readFromFile(localDir.resolveFile("project/dir1/dirFile.txt")), is("1234"));
+  }
+
+  private static void writeToFile(FileObject file, String content) throws Exception {
+    OutputStream out = file.getContent().getOutputStream();
+    IOUtils.write(content, out);
+    out.close();
+  }
+
+  private static String readFromFile(FileObject file) throws Exception {
+    InputStream in = file.getContent().getInputStream();
+    String content = IOUtils.toString(in);
+    in.close();
+    return content;
+  }
+
+  private static void writeToZipFile(FileObject zipFile, FileObject source) throws Exception {
+    zipFile.createFile();
+    ZipOutputStream zos = new ZipOutputStream(zipFile.getContent().getOutputStream());
+    for (FileObject file : source.findFiles(new FileTypeSelector(FileType.FILE))) {
+      String relativeName = file.getName().toString().substring(source.getName().toString().length() + 1);
+      ZipEntry zipEntry = new ZipEntry(relativeName);
+      InputStream in = file.getContent().getInputStream();
+      zos.putNextEntry(zipEntry);
+      IOUtils.copy(in, zos);
+      in.close();
+    }
+    zos.close();
+    zipFile.close();
   }
 
 }
